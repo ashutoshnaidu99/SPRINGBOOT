3bb9a6029b4e642e4805fa38dee1690c79c74f06
Ashutosh Naidu <136233470+ashutoshnaidu99@users.noreply.github.com>
2025-06-09 11:08:21 +0530

    Add files via upload

---
+++ Filesharingapp/src/main/java/com/example/Filesharingapp/FilesharingappApplication.java
@@ -0,0 +1,14 @@
+package com.example.Filesharingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesharingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesharingappApplication.class, args);
+	}
+
+}
+
